a7ba44a88cfb66fb6f7f963803ed0ba93e1707f8
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-25 22:25:54 +0300

    Creating a JWT

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const keys = require('../config/keys');
 
 const router = express.Router();
 const User = mongoose.model('User');
@@ -7,9 +9,15 @@ const User = mongoose.model('User');
 router.post('/signup', async (req, res) => {
   const { email, password } = req.body;
 
-  const user = new User({ email, password });
-  await user.save();
-  res.status(201).send('User Created!');
+  try {
+    const user = new User({ email, password });
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, keys.jwtSecretKey);
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
 });
 
 module.exports = router;
