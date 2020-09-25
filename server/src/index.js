e16edc9cf850a4e78ccddc6b403dd60286f799d8
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-25 22:58:02 +0300

    Wiring Up JSON Web Tokens

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const mongoConnection = require('./mongoDB/mongoose');
+const requiredAuth = require('./middlewares/requireAuth');
 
 mongoConnection();
 
@@ -11,6 +12,10 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(authRoutes);
+
+app.get('/', requiredAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
