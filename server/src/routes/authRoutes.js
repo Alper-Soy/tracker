a3ea6a0d611151250fc3989fb5271035b239c096
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-25 22:01:43 +0300

    Creating and Saving a User

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -4,9 +4,12 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const User = mongoose.model('User');
 
-router.post('/signup', (req, res) => {
-  console.log(req.body);
-  res.send('You singed up!');
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = new User({ email, password });
+  await user.save();
+  res.status(201).send('User Created!');
 });
 
 module.exports = router;
