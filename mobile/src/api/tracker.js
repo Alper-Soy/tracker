c521bbb1f9fdbe8c90e911e7e9d1ece353d4e82d
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-26 17:01:26 +0300

    Making an API Request for Signup

---
+++ mobile/src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: 'http://34b78b695806.ngrok.io',
+});
--- mobile/src/context/AuthContext.js
+++ mobile/src/context/AuthContext.js
@@ -1,5 +1,5 @@
-import { createContext } from 'react';
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, actions) => {
   switch (actions.type) {
@@ -8,8 +8,27 @@ const authReducer = (state, actions) => {
   }
 };
 
+const signup = (dispatch) => {
+  return async ({ email, password }) => {
+    try {
+      const response = await trackerApi.post('/signup', { email, password });
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {};
+};
+
+const signout = (dispatch) => {
+  return () => {};
+};
+
 export const { Context, Provider } = createDataContext(
   authReducer,
-  {},
+  { signin, signout, signup },
   { isSignedIn: false }
 );
