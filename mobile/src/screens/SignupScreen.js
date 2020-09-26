91b7acc621217a3d455dd322a681b15f21691c06
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-26 20:52:58 +0300

    Extracting from Logic

--- mobile/src/screens/SignupScreen.js
+++ mobile/src/screens/SignupScreen.js
@@ -1,44 +1,24 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Input, Text, Button } from 'react-native-elements';
-import Spacer from '../components/Spacer';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
   const { state, signup } = useContext(AuthContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
   return (
     <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up for Tracker</Text>
-      </Spacer>
-      <Input
-        keyboardType='email-address'
-        leftIcon={{ name: 'mail' }}
-        label='Email'
-        autoCapitalize='none'
-        autoCorrect={false}
-        value={email}
-        onChangeText={setEmail}
+      <AuthForm
+        headerText='Sign Up for Tracker'
+        errorMessage={state.errorMessage}
+        submitButtonText='Sign Up'
+        onSubmit={signup}
       />
-      <Spacer />
-      <Input
-        leftIcon={{ name: 'lock' }}
-        label='Password'
-        secureTextEntry={true}
-        autoCapitalize='none'
-        autoCorrect={false}
-        value={password}
-        onChangeText={setPassword}
+      <NavLink
+        routeName='Signin'
+        text='Already have an account? Sign in instead!'
       />
-      {state.errorMessage ? (
-        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-      ) : null}
-      <Spacer>
-        <Button title='Sign Up' onPress={() => signup({ email, password })} />
-      </Spacer>
     </View>
   );
 };
@@ -51,18 +31,9 @@ SignupScreen.navigationOptions = () => {
 
 const styles = StyleSheet.create({
   container: {
-    // borderColor: 'red',
-    // borderWidth: 10,
-    justifyContent: 'center',
     flex: 1,
-    marginBottom: 100,
-    // marginVertical: 50,
-  },
-  errorMessage: {
-    fontSize: 16,
-    color: 'red',
-    marginHorizontal: 15,
-    // marginVertical: 3,
+    justifyContent: 'center',
+    marginBottom: 50,
   },
 });
 
