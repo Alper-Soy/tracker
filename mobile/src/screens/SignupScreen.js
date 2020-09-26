1b861459b84288370a71d924e1a7111267dc86a0
Alper Soy <51034169+Alper-Soy@users.noreply.github.com>
2020-09-26 15:06:07 +0300

    Adding input props

--- mobile/src/screens/SignupScreen.js
+++ mobile/src/screens/SignupScreen.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Input, Text, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
-      <Input leftIcon={{ name: 'mail' }} label='Email' />
+      <Input
+        keyboardType='email-address'
+        leftIcon={{ name: 'mail' }}
+        label='Email'
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={email}
+        onChangeText={setEmail}
+      />
       <Spacer />
       <Input
         leftIcon={{ name: 'lock' }}
         label='Password'
         secureTextEntry={true}
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={password}
+        onChangeText={setPassword}
       />
       <Spacer>
         <Button title='Sign Up' />
@@ -35,9 +50,9 @@ const styles = StyleSheet.create({
     // borderColor: 'red',
     // borderWidth: 10,
     justifyContent: 'center',
-    flex: 1,
-    marginBottom: 100,
-    // marginTop:100
+    // flex: 1,
+    // marginBottom: 100,
+    marginVertical: 60,
   },
 });
 
